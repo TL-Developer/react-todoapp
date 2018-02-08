0af290a0ae5994b013c6271e3533d7fa907cacd8
Tiago Lima <tiago.jlima@yahoo.com.br>
2018-02-08 16:08:43 -0200

    App com react sem redux

---
+++ frontend/src/todo/todoList.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import IconButton from '../template/iconButton'
+
+export default props => {
+
+  const renderRows = () => {
+    const list = props.list || []
+    return list.map(todo => (
+      <tr key={todo._id}>
+        <td className={todo.done ? 'markedAsDone': ''}>{todo.description}</td>
+        <td>
+          <IconButton
+            hide={todo.done}
+            style="success"
+            icon="check"
+            onClick={() => props.handleMarkAsDone(todo)}
+          />
+          <IconButton
+            hide={!todo.done}
+            style="warning"
+            icon="undo"
+            onClick={() => props.handleMarkAsPending(todo)}
+          />
+          <IconButton
+            hide={!todo.done}
+            style="danger"
+            icon="trash-o"
+            onClick={() => props.handleRemove(todo)}
+          />
+        </td>
+      </tr>
+    ));
+  }
+
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Descrição</th>
+          <td className="tableActions"></td>
+        </tr>
+      </thead>
+      <tbody>
+        {renderRows()}
+      </tbody>
+    </table>
+  )
+}
